71e3c0aa0613166e18879100199e5804c5ed6012
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-28 10:13:11 -0600

    debug

--- controllers/user.js
+++ controllers/user.js
@@ -1,29 +1,69 @@
+// Importar dependencias y modulos
+const User = require("../models/user");
+
 // Acciones de prueba
-const pruebaUser = (req,res) => {
-    return res.status(200).send({
-        message: "Mensaje enviado desde: controllers/user.js"
-    });
-}
+const pruebaUser = (req, res) => {
+  return res.status(200).send({
+    message: "Mensaje enviado desde: controllers/user.js",
+  });
+};
 
 // Registro de usuarios
-const register = (req,res) =>{
-    // Recoger datos de la peticion
+const register =  (req, res) => {
+  // Recoger datos de la peticion
+  let params = req.body;
 
-    // Comprobar que me llegan bien (+ validacion)
+  // Comprobar que me llegan bien (+ validacion)
+  if (!params.name || !params.email || !params.password || !params.nick) {
+    return res.status(400).json({
+        status: "error",
+        message: "Faltan datos por enviar",
+    });
+  }
 
-    // Control de usuarios duplicados
+    // Crear objeto de usuario
+    let user_to_save = new User(params)
 
-    // Cifrar la contraseña
+    // control de usuarios duplicados
+    User.find({
+        $or: [
+            { email: user_to_save.email.toLowerCase() },
+            { nick: user_to_save.nick.toLowerCase() },
+        ]
+ 
+    }).then((users) => {       
+        // si existe un usuario con el mismo nick o email
+        if (users && users.length >= 1) {
+            return res.status(200).send({
+                status: "success",
+                message: "El usuario ya existe",
+            });
+        }
+ 
+        // cifrar la contraseña        
+ 
+        // guardar usuario bd
+ 
+        // devolver resultado
+        return res.status(200).json({
+            status: "success",
+            message: "Accion de registro de usuarios",
+            // params,
+            user_to_save,
+        });
+ 
+    }).catch((error) => {
+        // si llega un error
+        if (error)
+            return res.status(500).json({
+                status: "error",
+                message: "Error en la consulta de usuarios",
+            });
+    });
 
-    // Guardar usuario en la BD
-
-    // Devolver resultado
-    return res.status(200).json({
-        message: "Metodo de accion de registro de usuarios"
-    })
-}
+};
 
 module.exports = {
-    pruebaUser,
-    register
-}
+  pruebaUser,
+  register,
+};
