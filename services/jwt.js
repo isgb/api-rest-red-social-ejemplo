e21299cc3a35a03b6c99dedf98eb1fcff1b7b0f6
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-28 11:28:28 -0600

    debug

---
+++ services/jwt.js
@@ -0,0 +1,23 @@
+// Importar dependencias
+const jet = require("jwt-simple")
+const moment = require("moment")
+
+// Generar clave secreta
+const secret = "CLAVE_SECRETA_del_proyecto_DE_LA_RED_social_987987";
+
+// Crear una función para generar tokens
+const createToken = (user) => {
+    const payload = {
+        id: user._id,
+        name: user.name,
+        surname: user.surname,
+        nick: user.surname,
+        email: user.email,
+        role: user.role,
+        imagen: user.image,
+        iat: moment().unix(),
+        exp: moment().add(30, "days").unix
+    }
+}
+
+// Devolver jwt token codificado
