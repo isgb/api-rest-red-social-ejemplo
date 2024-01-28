762a4556bcc22184b310bd1aacaabd093852ad1f
isaias_pruebas <isgb_codigo@hotmail.com>
2024-01-28 09:34:04 -0600

    debug

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,25 @@ const pruebaUser = (req,res) => {
     });
 }
 
+// Registro de usuarios
+const register = (req,res) =>{
+    // Recoger datos de la peticion
+
+    // Comprobar que me llegan bien (+ validacion)
+
+    // Control de usuarios duplicados
+
+    // Cifrar la contraseña
+
+    // Guardar usuario en la BD
+
+    // Devolver resultado
+    return res.status(200).json({
+        message: "Metodo de accion de registro de usuarios"
+    })
+}
+
 module.exports = {
-    pruebaUser
+    pruebaUser,
+    register
 }
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const UserController = require("../controllers/user");
 
 // Definir rutas
 router.get("/prueba-usuario", UserController.pruebaUser);
+router.get("/register", UserController.register)
 
 // Exportar router
 module.exports = router;
