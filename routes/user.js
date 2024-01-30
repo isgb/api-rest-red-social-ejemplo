a5cfc246a34ac21369ffaf38d46ab99f8decc967
oscar <oscar.vasquez@zentro.com.mx>
2024-01-30 16:08:19 -0600

    debug

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
         cb(null,"./uploads/avatars/")
     },
     filename:  (req,file,cb)=>{
-        cb(null,"avatar-"+Date.now()+"-"+file.originalName)
+        cb(null,"avatar-"+Date.now()+"-"+file.originalname)
     }
 })
 
