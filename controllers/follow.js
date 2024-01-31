32ff14b71eb2aee5554f4bf5b7f6517cbc084764
oscar <oscar.vasquez@zentro.com.mx>
2024-01-31 16:12:54 -0600

    debug

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,3 +1,7 @@
+// Importar modelo
+const Follow = require('../models/follow');
+const User = require("../models/user");
+
 // Acciones de prueba
 const pruebaFollow = (req,res) => {
     return res.status(200).send({
